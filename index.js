f8de5ddb2b613437b66abe1806bc5266120691b6
Adriel Puello <adrielpuello@gmail.com>
2022-08-30 15:40:42 -0400

    commit tree 899a4d1e0ffeec431305be5d3d34859e16dfe5dc

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solutions in this file
+const name1 = 'Guadalupe'
+const name2 = 'Ollie'
+const name3 = 'Aki'
+const surprise = [`Thank you, ${name1}, for the wonderful surprise gift!`,
+`Thank you, ${name2}, for the wonderful surprise gift!`,
+`Thank you, ${name3}, for the wonderful surprise gift!`,]
+function writeCards(){
+    return surprise
+}
+function startCountdown(seconds) {
+    let counter = seconds;
+      
+    const interval = setInterval(() => {
+      console.log(counter);
+      counter--;
+        
+      if (counter < 0 ) {
+        clearInterval(interval);
+        
+      }
+    }, 1000);
+  }
+  
+  function countDown(seconds){
+    let counter = seconds;
+
+    while (counter >= 0) {
+        console.log(counter--);
+    if (counter < 0){
+        clearInterval(counter);
+        }
+      }
+  }
+  
+ 
